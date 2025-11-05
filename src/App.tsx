45a366be4a914d0686a22971f525e739dd770771
tjay1760 <mcjthiongo@gmail.com>
2025-11-05 18:42:15 +0300

    interface quantities

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-
-import './App.css'
-
+import "./App.css";
 
 function App() {
+  interface Quantity {
+    amount: number;
+  }
 
+  const displayQuantity = (qty: Quantity): void => {
+    console.log(`The quantity is: ${qty.amount}`);
+  };
+
+  const lives: Quantity = { amount: 5 };
+  const population= { country: "Kenya", amount: 7800000000 };
+  const planets= { solarSystem: "Milky Way", amount: 8 };
+  displayQuantity(population);
   return (
     <>
-
+      <h1>The app</h1>
+      <div> Lives: {lives.amount}</div>
+      <div> Population: {population.amount}</div>
+      <div> Planets: {planets.amount}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
